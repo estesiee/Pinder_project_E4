0ea8ee0be0fe59ec103c59a49a70d33f300446de
P4Crl <paul.courtial@edu.esiee.fr>
2022-10-27 15:17:21 +0200

    Modification Test Bouton

--- app/src/main/java/com/example/pinder_project_e4/MainActivity.java
+++ app/src/main/java/com/example/pinder_project_e4/MainActivity.java
@@ -2,23 +2,14 @@ package com.example.pinder_project_e4;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,5 +27,11 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.wallpaper);
         keep = (Button) findViewById(R.id.keep);
         kick = (Button) findViewById(R.id.kick);
+        keep.setOnClickListener(new View.OnClickListener() {
+          @Override
+                  public void onClick(View view) {
+              System.out.println("Keep");
+          }
+        });
     }
 }
