7b5a55d03ec7ae701afc4bc4dda70ddd9eb3c7a2
P4Crl <paul.courtial@edu.esiee.fr>
2022-10-27 15:41:53 +0200

    Addons ImageButton

--- app/src/main/java/com/example/pinder_project_e4/MainActivity.java
+++ app/src/main/java/com/example/pinder_project_e4/MainActivity.java
@@ -8,14 +8,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import androidx.navigation.ui.AppBarConfiguration;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
-    Button keep;
-    Button kick;
+    ImageButton love;
+    ImageButton remove;
 
     private AppBarConfiguration appBarConfiguration;
 
@@ -25,14 +26,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.home_page);
 
         imageView = (ImageView) findViewById(R.id.wallpaper);
-        //this.imageView.setImageResource(R.drawable.tokyowallpaper);
-        keep = (Button) findViewById(R.id.keep);
-        kick = (Button) findViewById(R.id.kick);
-        keep.setOnClickListener(new View.OnClickListener() {
+        love = (ImageButton) findViewById(R.id.love);
+        remove = (ImageButton) findViewById(R.id.remove);
+        love.setOnClickListener(new View.OnClickListener() {
           @Override
                   public void onClick(View view) {
               System.out.println("Keep");
           }
+        });
+
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Kick");
+            }
         });
     }
 }
