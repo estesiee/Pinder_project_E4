764ed0757912d54013d9c34082d6b7128b5081fe
P4Crl <paul.courtial@edu.esiee.fr>
2022-10-27 15:07:11 +0200

    Version update : plus propre

--- app/src/main/java/com/example/pinder_project_e4/MainActivity.java
+++ app/src/main/java/com/example/pinder_project_e4/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,6 +22,10 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    Button keep;
+    Button kick;
+
     private AppBarConfiguration appBarConfiguration;
 
     @Override
@@ -26,34 +33,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        imageView = (ImageView) findViewById(R.id.wallpaper);
+        keep = (Button) findViewById(R.id.keep);
+        kick = (Button) findViewById(R.id.kick);
     }
 }
