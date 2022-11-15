441a8f726ca41837ac5e6d03af42dc90bdb398f6
estesiee <esteban.brisson@edu.esiee.fr>
2022-11-15 16:47:07 +0100

    button return home work

--- app/src/main/java/com/example/pinder_project_e4/Gallery.java
+++ app/src/main/java/com/example/pinder_project_e4/Gallery.java
@@ -1,19 +1,36 @@
 package com.example.pinder_project_e4;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
 
 public class Gallery extends AppCompatActivity {
+
+    ImageButton toHome;
+
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gallery_page);
 
+        Intent intentHome = new Intent(this,MainActivity.class);
+
+        toHome = (ImageButton) findViewById(R.id.toHome);
         System.out.println("J'ai changé");
+
+        toHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(intentHome);
+            }
+        });
     }
 }
